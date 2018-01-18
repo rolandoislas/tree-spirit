1785bb22e29ab9c86aa0a329668a9af54df16eee
Rolando Islas <admin@rolandoislas.com>
2018-01-18 14:19:58 -0700

    Fix doors dropping two items

--- src/main/java/com/rolandoislas/treespirit/block/BlockSpiritDoor.java
+++ src/main/java/com/rolandoislas/treespirit/block/BlockSpiritDoor.java
@@ -7,14 +7,12 @@ import net.minecraft.block.BlockDoor;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 /**
@@ -32,13 +30,15 @@ public class BlockSpiritDoor extends BlockDoor {
 
 	@Override
 	public ItemStack getItem(World worldIn, BlockPos pos, IBlockState state) {
-		return new ItemStack(ModItems.DOOR);
+		return new ItemStack(getItem());
+	}
+
+	private Item getItem() {
+		return ModItems.DOOR;
 	}
 
 	@Override
-	public List<ItemStack> getDrops(IBlockAccess world, BlockPos pos, IBlockState state, int fortune) {
-		ArrayList<ItemStack> drops = new ArrayList<ItemStack>();
-		drops.add(new ItemStack(ModItems.DOOR));
-		return drops;
+	public Item getItemDropped(IBlockState state, Random rand, int fortune) {
+		return state.getValue(HALF).equals(BlockDoor.EnumDoorHalf.UPPER) ? Items.AIR : getItem();
 	}
 }
